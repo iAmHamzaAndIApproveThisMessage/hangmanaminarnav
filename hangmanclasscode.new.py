620acf852c5fce943483f28861a8f60df1b4821d
iAmHamzaAndIApproveThisMessage <amish22@students.shrewsbury.k12.ma.us>
2020-02-27 13:36:11 -0500

    Update hangmanclasscode.new.py

--- hangmanclasscode.new.py
+++ hangmanclasscode.new.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+
 word = ""
 letter = ""
 updatedSpaces = []
@@ -23,6 +24,26 @@ def won():
                          
 def main():
     initialize()
-    getLetter()                
+    getLetter()
+    check(letter)                
 
-main()                      
+
+
+bupdatedSpaces=['-','-','-','-','-']
+word='steve'
+lives=5
+def check(letter):
+    global lives
+    global updatedSpaces
+    global word
+    if letter in word:
+        for i in range(0,len(word)-1):
+            Hamza=word.find(letter, i, len(word))
+            if Hamza != -1:
+                updatedSpaces[Hamza]=letter
+    else:
+        lives=lives-1
+        print('Not in word,', lives, 'lives left!')
+    print(updatedSpaces)
+
+main()
